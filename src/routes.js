99453379b1f3457ab96840ae21069b249801ccf9
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-25 23:48:00 -0300

    Criação das rotas para acessar as paginas

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const routes = express.Router();
+
+const views = __dirname + "/views/";
+
+const profile = {
+  name: "Johnbrow",
+  avatar:
+    "https://lh3.googleusercontent.com/a-/AOh14GiIc7ZIPSQuhP3YOlUoUusgxzL4rispaiiRTKMUqw=s96-c-rg-br100",
+  "monthly-budget": 3000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4,
+};
+
+//  req e res
+routes.get("/", (req, res) => {
+  return res.render(views + "index");
+});
+routes.get("/job", (req, res) => {
+  return res.render(views + "job");
+});
+routes.get("/job/edit", (req, res) => {
+  return res.render(views + "job-edit");
+});
+routes.get("/profile", (req, res) => {
+  return res.render(views + "profile", { profile });
+});
+
+module.exports = routes;
