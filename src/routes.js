fc2d2507141558e1e4d74c72b345c5cc48322f02
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-31 11:28:34 -0300

    Ajustes de dados no index

--- src/routes.js
+++ src/routes.js
@@ -14,15 +14,44 @@ const profile = {
   "vacation-per-year": 4,
 };
 
+const jobs = [
+  {
+    id: 1,
+    name: "Pizzaria Gulloso",
+    "daily-hours": 2,
+    "total-hours": 60,
+    created_at: Date.now(),
+  },
+
+  {
+    id: 2,
+    name: "OneTwo Project",
+    "daily-hours": 3,
+    "total-hours": 47,
+    created_at: Date.now(),
+  },
+];
+
 //  req e res
 routes.get("/", (req, res) => {
-  return res.render(views + "index");
+  return res.render(views + "index", { jobs });
 });
 routes.get("/job", (req, res) => {
   return res.render(views + "job");
 });
 routes.post("/job", (req, res) => {
-  console.log(req.body);
+  //  gambiarra linda kkkkk e para achar um id dependendo da posição do array
+  const lastId = jobs[jobs.length - 1]?.id || 1;
+
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    "daily-hours": req.body["daily-hours"],
+    "total-hours": req.body["total-hours"],
+    created_at: Date.now(),
+  });
+
+  return res.redirect("/");
 });
 routes.get("/job/edit", (req, res) => {
   return res.render(views + "job-edit");
