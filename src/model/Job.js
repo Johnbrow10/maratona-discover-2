8a587bdcc07bb01662162acaf887062a106d37ab
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-08 17:20:09 -0300

    Criando o model Job refatorando de routes e deixando independente

---
+++ src/model/Job.js
@@ -0,0 +1,29 @@
+let data = [
+  {
+    id: 1,
+    name: "Pizzaria Gulloso",
+    "daily-hours": 2,
+    "total-hours": 2,
+    created_at: Date.now(),
+  },
+
+  {
+    id: 2,
+    name: "OneTwo Project",
+    "daily-hours": 3,
+    "total-hours": 47,
+    created_at: Date.now(),
+  },
+];
+
+module.exports = {
+  get() {
+    return data;
+  },
+  update(newJob) {
+    data = newJob;
+  },
+  delete(id) {
+    data = data.filter((job) => Number(job.id) !== Number(id));
+  },
+};
