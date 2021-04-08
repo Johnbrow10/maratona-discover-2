2c78bbc4f2370307057ea1af3588d74ed42d7de7
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-08 17:21:16 -0300

    E refatorando os controllers de routes e deixando independente como os metodos de create, show, update e delete no JobController

---
+++ src/controllers/JobController.js
@@ -0,0 +1,98 @@
+const Job = require("../model/Job");
+const JobUtils = require("../utils/JobUtils");
+const Profile = require("../model/Profile");
+
+module.exports = {
+  index(req, res) {
+    const jobs = Job.get();
+    const profile = Profile.get();
+
+    const updateJobs = jobs.map((job) => {
+      const remaining = JobUtils.remainingDays(job);
+      const status = remaining <= 0 ? "done" : "progress";
+
+      return {
+        ...job,
+        remaining,
+        status,
+        budget: JobUtils.calculateBudget(job, profile["value-hour"]),
+      };
+    });
+
+    return res.render("index", { jobs: updateJobs });
+  },
+
+  save(req, res) {
+    const jobs = Job.get();
+
+    //  gambiarra linda kkkkk e para achar um id dependendo da posição do array
+    const lastId = jobs[jobs.length - 1]?.id || 0;
+
+    jobs.push({
+      id: lastId + 1,
+      name: req.body.name,
+      "daily-hours": req.body["daily-hours"],
+      "total-hours": req.body["total-hours"],
+      created_at: Date.now(), // Atribuindo data de hoje
+    });
+
+    return res.redirect("/");
+  },
+
+  create(req, res) {
+    return res.render("job");
+  },
+
+  show(req, res) {
+    const jobs = Job.get();
+    const jobId = req.params.id;
+    const profile = Profile.get();
+
+    const job = jobs.find((job) => Number(job.id) === Number(jobId));
+
+    if (!job) {
+      return res.send("Job nots Found!");
+    }
+
+    job.budget = JobUtils.calculateBudget(job, profile["value-hour"]);
+
+    return res.render("job-edit", { job });
+  },
+
+  update(req, res) {
+    const jobs = Job.get();
+    const jobId = req.params.id;
+
+    const job = jobs.find((job) => Number(job.id) === Number(jobId));
+
+    if (!job) {
+      return res.send("Job nots Found!");
+    }
+
+    const updatedJob = {
+      ...job,
+      name: req.body.name,
+      "total-hours": req.body["total-hours"],
+      "daily-hours": req.body["daily-hours"],
+    };
+
+    const newJobs = jobs.map((job) => {
+      if (Number(job.id) === Number(jobId)) {
+        job = updatedJob;
+      }
+
+      return job;
+    });
+
+    Job.update(newJobs);
+
+    res.redirect("/job/" + jobId);
+  },
+  delete(req, res) {
+    const jobId = req.params.id;
+
+    Job.delete(jobId);
+
+    return res.redirect("/");
+  },
+};
