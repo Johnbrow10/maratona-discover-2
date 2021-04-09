1329757add4b6528351230480380fc3e313b3fe6
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-09 16:11:24 -0300

    Criando calculo de horas livres para trabalhar nos projetos em progresso

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -11,13 +11,19 @@ module.exports = {
       done: 0,
       total: jobs.length,
     };
+    // Total de horas por dia de cada trabalho em progresso
+    let jobTotalHours = 0;
 
     const updateJobs = jobs.map((job) => {
       const remaining = JobUtils.remainingDays(job);
       const status = remaining <= 0 ? "done" : "progress";
 
-      //    Dependendo se for done ou progress ele sempre vai adiocionar +1 para cada status
+      //Dependendo se for done ou progress ele sempre vai adicionar +1 para cada status
       statusCount[status] += 1;
+
+      // Dependendo se o status tiver como progress ele ira adicionar
+      // no jobTotalHours as horas disponiveis para trabalhar no dia
+      if (status == "progress") jobTotalHours += Number(job["daily-hours"]);
 
       return {
         ...job,
@@ -27,10 +33,15 @@ module.exports = {
       };
     });
 
+    //  Qtd de horas que quero trabalhar por dia MENOS
+    // a quantidade de horas / dia de cada job em progresso
+    const freeHours = profile["hours-per-day"] - jobTotalHours;
+
     return res.render("index", {
       jobs: updateJobs,
       profile: profile,
       statusCount: statusCount,
+      freeHours: freeHours,
     });
   },
 };
