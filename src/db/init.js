d593e2b2353cae05450aca5611e60e6894a79003
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-10 23:21:23 -0300

    Inserindo os dados de profile com valores

--- src/db/init.js
+++ src/db/init.js
@@ -1,4 +1,5 @@
 const Database = require("config");
+const Profile = require("../model/Profile");
 
 Database();
 
@@ -6,19 +7,35 @@ Database.exec(`CREATE TABLE profile (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     avatar TEXT,
-    monthly-budget INT ,
-    days-per-week INT,
-    hours-per-day INT,
-    vacation-per-day INT,
-    value-hour INT
+    monthly_budget INT ,
+    days_per_week INT,
+    hours_per_day INT,
+    vacation_per_day INT,
+    value_hour INT
 )`);
 
 Database.exec(`CREATE TABLE jobs(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    daily-hours INT,
-    total-hours INT,
-    created-at DATETIME
+    daily_hours INT,
+    total_hours INT,
+    created_at DATETIME
 )`);
+
+Database.run(`INSERT INTO profile(
+    name,
+    avatar,
+    monthly_budget,
+    days_per_week,
+    hours_per_day,
+    vacation_per_day,
+) VALUES(
+    "Johnbrow",
+    "https://github.com/johnbrow10.png",
+    3000,
+    5,
+    5,
+    4,
+);`);
 
 Database.close();
