3348f9a913b487ef6d9d1f6f6c1146364c2f70fa
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-13 10:16:16 -0300

    Recriando o model de Job com a conexao ao bd com as funcoes de listar e criar feitas

--- src/model/Job.js
+++ src/model/Job.js
@@ -1,32 +1,48 @@
-let data = [
-  {
-    id: 1,
-    name: "Pizzaria Gulloso",
-    "daily-hours": 2,
-    "total-hours": 2,
-    created_at: Date.now(),
-  },
+const Database = require("../db/config");
 
-  {
-    id: 2,
-    name: "OneTwo Project",
-    "daily-hours": 3,
-    "total-hours": 47,
-    created_at: Date.now(),
-  },
-];
 
 module.exports = {
-  get() {
-    return data;
+  async get() {
+    const db = await Database();
+
+    const jobs = await db.all(`SELECT * FROM jobs `);
+
+    await db.close();
+
+    // Criando um novo retorno de dados para a controller
+    return jobs.map((job) => ({
+      id: job.id,
+      name: job.name,
+      "daily-hours": job.daily_hours,
+      "total-hours": job.total_hours,
+      created_at: job.created_at,
+    }));
   },
+
   update(newJob) {
     data = newJob;
   },
+
   delete(id) {
     data = data.filter((job) => Number(job.id) !== Number(id));
   },
-  create(newJob) {
-    data.push(newJob);
+
+  async create(newJob) {
+    const db = await Database();
+
+    // quando fazer insert into os valores dos campos de string vao ser em volta de aspas
+    await db.run(`INSERT INTO jobs (
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES(
+      "${newJob.name}",
+      ${newJob["daily-hours"]},
+      ${newJob["total-hours"]},
+      ${newJob.created_at}
+    )`);
+
+    await db.close();
   },
 };
